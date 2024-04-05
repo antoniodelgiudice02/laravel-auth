42d7ebdb833159f1de9838976d5bc989a95e7995
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-04-05 20:01:12 +0200

    add model project msrc, make seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class UserSeeder extends Seeder
         $user = new User;
         $user->name = 'Antonio';
         $user->email = 'antodelgiu02@gmail.com';
-        $user->password = 'password';
+        $user->password = Hash::make('password');
 
         $user->save();
     }
