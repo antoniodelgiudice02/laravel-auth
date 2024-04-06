27ce5fbf66c9f39d97e6e08bbd2bb82b7ef72d88
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-04-06 16:56:32 +0200

    add delete CRUD

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -18,6 +18,8 @@
                 <th>Contenuto</th>
                 <th>Slug</th>
                 <th></th>
+                <th></th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
@@ -28,10 +30,50 @@
                     <td>{{ $project->title }}</td>
                     <td>{{ $project->getContent(50) }}</td>
                     <td>{{ $project->slug }}</td>
+
+                    <!-- VISUALIZZA -->
                     <td>
                         <a class='btn btn-success' href="{{ route('admin.projects.show', $project)}}"><i class="fa-solid fa-eye"></i></a>
+                    </td>
+
+                    <!-- MODIFICA -->
+                    <td>
                         <a class='btn btn-success' href="{{ route('admin.projects.edit', $project)}}"><i class="fa-solid fa-pen-nib"></i></a>
-                        <a class='btn btn-danger' href="{{ route('admin.projects.destroy', $project)}}"><i class="fa-solid fa-trash-can"></i></a>
+                    </td>
+
+                    <!-- ELIMINA -->
+                    <td>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-{{$project->id}}">
+                        <i class="fa-solid fa-trash-can"></i>
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="delete-{{$project->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminare {{ $project->title }}?</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Se elimini questo progetto non potrai più recuperarlo.
+                                    </div>
+                                    <div class="modal-footer">
+                                        
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+
+                                        <form action="{{ route('admin.projects.destroy', $project)}}" method='POST'>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class='btn btn-danger'>Elimina</button>
+                                        </form>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </td>
                 </tr>
             @empty
